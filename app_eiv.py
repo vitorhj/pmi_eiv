51bc385a1294bf4e62890266cfeac801286bee9d
vitorhj <90215531+vitorhj@users.noreply.github.com>
2023-02-03 16:28:01 -0300

    Update app_eiv.py

--- app_eiv.py
+++ app_eiv.py
@@ -86,7 +86,7 @@ try:
         #nova_tabela3.drop([0], axis=1, inplace=True)
         #nova_tabela
         
-        st.text(inscricao_input)
+        st.text(str(inscricao_input))
         
 except:
   pass
