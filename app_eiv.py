57618f362ee5075dbb6bb13fd3fa38a502096a13
vitorhj <90215531+vitorhj@users.noreply.github.com>
2023-02-03 15:16:00 -0300

    Update app_eiv.py

--- app_eiv.py
+++ app_eiv.py
@@ -13,9 +13,17 @@ from PIL import Image
 ## INPUTS DA PÁGINA DO STREAMLIT ##
 
 st.set_page_config(
-     page_title="PMI - Empresas Alvará de Funcionamento",
+     page_title="PMI - EIV",
      page_icon=('./dados/favicon.png'),
      layout="wide",
  )
 
 #______________________________________________________________________________________________________________________
+
+## SIDEBAR ##
+
+# Input box do aprova
+logo_image = ('./dados/logo.png')
+st.sidebar.image(logo_image, width=200)
+st.sidebar.subheader('Verificação EIV:')
+texto_aprova = st.sidebar.text_input('Inscrição Imobiliária:','',key="inputbox1")
