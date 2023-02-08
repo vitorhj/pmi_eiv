29cf34bb42d967bdbf0f78d1abed0851e5794205
vitorhj <90215531+vitorhj@users.noreply.github.com>
2023-02-08 14:39:31 -0300

    Update app_eiv.py

--- app_eiv.py
+++ app_eiv.py
@@ -117,7 +117,7 @@ try:
         tabela_verificacao = tabela_anexoeiv.merge(tabela_parametros_uso,left_on='USO/ ATIVIDADE', right_on='ATIVIDADE/ USO')
         tabela_verificacao=tabela_verificacao.loc[::,['ITEM', 'USO/ ATIVIDADE', 'ZONEAMENTO', 'Ac > (m²)','At > (m²)', 'UH', 'OBSERVAÇÃO']]
         tabela_verificacao
-        tabela_verificacao['ITEM'].unique()
+        tabela_verificacao.ITEM.unique()
      
         st.subheader('Todas as regras de EIV para o zoneamento:')
         tabela_anexoeiv = tabela_anexoeiv.merge(tabela_filtrada,left_on='ZONEAMENTO', right_on='nome')
